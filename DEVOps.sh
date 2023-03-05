be1457a7be756028353a59320e232a4c4db272a0
Jeroen Heymans <heymansjeroen@proximus.be>
2023-03-05 11:50:00 +0100

    Eerste bestanden en mappen aangemaakt.  Eerste succesvolle test met bash-script OK

---
+++ DEVOps.sh
@@ -0,0 +1,50 @@
+#! /bin/bash
+
+echo '
+*********************************
+*        DEVOPS PROJECT         *
+*      NEXTCLOUD ON DOCKER      *
+*      ===================      *
+*                               *
+*    Automatische Installatie   *
+*********************************
+'
+
+echo "Dit script dient als root-gebruiker uitgevoerd te worden:"
+sudo echo "script kan verder gaan ..."
+
+echo '
+Controleren op noodzakelijke toepassingen ...
+'
+
+sudo apt-get install git ansible -y > /dev/null &
+
+# Display spinning during install of packages
+pid=$! # Process Id of the previous running command
+
+spin='-\|/'
+
+i=0
+while kill -0 $pid 2>/dev/null
+do
+  i=$(( (i+1) %4 ))
+  printf "\r${spin:$i:1}"
+  sleep .1
+done
+
+echo '
+
+Klaar!
+
+'
+
+DIRECTORY=/home/$USER/DEVOPS_PROJ
+
+{ if [ -d "$DIRECTORY" ]; then
+  rm -rf /home/$USER/DEVOPS_PROJ/
+  git clone https://github.com/jeroen29051980/DEVOPS_PROJ.git /home/$USER/DEVOPS_PROJ
+fi
+
+if [ ! -d "$DIRECTORY" ]; then
+  git clone https://github.com/jeroen29051980/DEVOPS_PROJ.git /home/$USER/DEVOPS_PROJ
+fi } &> /dev/null
