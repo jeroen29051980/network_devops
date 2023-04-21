321091e7df2eedf0b80bffa8543116fc76533796
Jeroen Heymans <heymansjeroen@proximus.be>
2023-04-21 17:54:05 +0200

    Aanpassing One-click-install

--- DEVOps.sh
+++ DEVOps.sh
@@ -55,3 +55,6 @@ fi
 if [ ! -d "$DIRECTORY" ]; then
   git clone https://github.com/jeroen29051980/DEVOPS_PROJ.git /home/$USER/DEVOPS_PROJ
 fi } &> /dev/null
+
+echo -e "Het script gaat nu verder met het opzetten van de virtuele machines \n"
+echo "Opgelet: Momenteel werkt dit script enkel met 
